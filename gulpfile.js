3bb75217303fbe2b4800ba90750c706e28c19b80
Yura Kholodov <yurdast@mail.ru>
2024-08-14 17:01:20 +0300

    14.08.24

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,25 @@
-const { src, dest, watch, parallel, series } = require("gulp");
+const gulp = require('gulp');
 
-const scss = require("gulp-sass")(require("sass"));
-const concat = require("gulp-concat");
-const uglify = require("gulp-uglify-es").default;
-const browserSync = require("browser-sync").create();
-const autoprefixer = require("gulp-autoprefixer");
-const clean = require("gulp-clean");
+// Tasks
+require('./gulp/dev.js');
+require('./gulp/docs.js');
+require('./gulp/fontsDev.js');
+require('./gulp/fontsDocs.js');
 
-function scripts() {
-  return src(["app/js/main.js"])
-    .pipe(concat("main.min.js"))
-    .pipe(uglify())
-    .pipe(dest("app/js"))
-    .pipe(browserSync.stream());
-}
+gulp.task(
+	'default',
+	gulp.series(
+		'clean:dev', 'fontsDev',
+		gulp.parallel('html:dev', 'sass:dev', 'images:dev', gulp.series('svgStack:dev', 'svgSymbol:dev'), 'files:dev', 'js:dev'),
+		gulp.parallel('server:dev', 'watch:dev')
+	)
+);
 
-function styles() {
-  return src("app/scss/style.scss")
-    .pipe(autoprefixer({ overrideBrowserslist: ["last 10 version"] }))
-    .pipe(concat("style.min.css"))
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(dest("app/css"))
-    .pipe(browserSync.stream());
-}
-
-function watching() {
-  watch(["app/scss/style.scss"], styles);
-  watch(["app/js/main.js"], scripts);
-  watch(["app/*.html"]).on("change", browserSync.reload);
-}
-
-function browsersync() {
-  browserSync.init({
-    server: {
-      baseDir: "app/",
-    },
-  });
-}
-
-function cleanDist() {
-  return src("dist").pipe(clean());
-}
-
-function building() {
-  return src(["app/css/style.min.css", "app/js/main.min.js", "app/**/*.html"], {
-    base: "app",
-  }).pipe(dest("dist"));
-}
-
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watching = watching;
-exports.browsersync = browsersync;
-exports.build = series(cleanDist, building);
-
-exports.default = parallel(styles, scripts, browsersync, watching);
+gulp.task(
+	'docs',
+	gulp.series(
+		'clean:docs', 'fontsDocs',
+		gulp.parallel('html:docs', 'sass:docs', 'images:docs', gulp.series('svgStack:docs', 'svgSymbol:docs'), 'files:docs', 'js:docs'),
+		gulp.parallel('server:docs')
+	)
+);
